d0d7bc922ca5da9513ebc9eb8f02a79d9e4c4b77
Daniel Britt <dbritt89@gmail.com>
2022-07-21 00:50:49 -0600

    almost working. user input is truncated for some reason. to be sorted another day.

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+var user_url string
+var ytdlp_path string
+var user_home string
+
+func ytdlp_check() (string, bool) {
+	path, err := exec.LookPath("yt-dlp")
+	if err != nil {
+		log.Fatal("yt-dlp was not found in your path. Is it installed?")
+		return path, false
+	}
+	fmt.Printf("yt-dlp is available at %s\n", path)
+	return path, true
+}
+
+func get_input() string {
+	fmt.Println("Enter the URL of the YouTube Video to download:")
+	fmt.Scanf("%s", user_url)
+	return user_url
+}
+
+func downloader(string) {
+	user_home, _ := os.UserHomeDir()
+	cmd := exec.Command("yt-dlp " + user_url)
+	cmd.Dir = user_home + "/Videos/"
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+}
+
+func main() {
+	if _, ytdlp_exists := ytdlp_check(); ytdlp_exists == true {
+		get_input()
+		downloader(user_url)
+	}
+
+}
